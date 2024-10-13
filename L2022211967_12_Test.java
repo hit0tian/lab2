a8c830e6ac5d1f79e50bf1e71e108ba9305db635
Eric <erichao0612@outlook.com>
2024-10-13 09:11:28 +0800

    soft candy

--- L2022211967_12_Test.java
+++ L2022211967_12_Test.java
@@ -2,15 +2,23 @@ import static org.junit.Assert.*;
 // 使用等价类划分原则
 public class SolutionTest {
 
+    //我建议一个test写一个用例，一位乘写一个，多为乘写一个，
 //测试乘法，多位乘和一位乘是否正常
 // 例1："2"*"3"="6"
 // 例2："123"*"456"="56088"
     @org.junit.Test
-    public void testMultiply() {
+    public void testMultiply1() {
     Solution solution = new Solution();
     String ans = solution.multiply("2","3");
     assertEquals("6",ans);
-     ans = solution.multiply("123","456");
+
+    }
+
+    @org.junit.Test
+    public void testMultiply2() {
+    Solution solution = new Solution();
+    
+    String ans = solution.multiply("123","456");
     assertEquals("56088",ans);
 
     }
@@ -20,11 +28,17 @@ public class SolutionTest {
 // 例2："9"+"2"="11"
 
     @org.junit.Test
-    public void addStrings() {
+    public void addStrings1() {
         Solution solution = new Solution();
         String ans = solution.addStrings("2","3");
         assertEquals("5",ans);
-        ans = solution.addStrings("9","2");
+        
+    }
+    @org.junit.Test
+    public void addStrings2() {
+        Solution solution = new Solution();
+        
+        String ans = solution.addStrings("9","2");
         assertEquals("11",ans);
     }
 }
